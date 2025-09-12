8e9303f2f831e14045ec9ee86c7e5c2d7e20387c
sb-higa <motoki.higa@sbisecsol.com>
2025-09-12 19:10:45 +0900

    fix: インスタンスサイズ変更w

--- develop/vm.tf
+++ develop/vm.tf
@@ -59,7 +59,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
   location                        = azurerm_resource_group.develop.location
   resource_group_name             = azurerm_resource_group.develop.name
   network_interface_ids           = [azurerm_network_interface.develop_nic.id]
-  size                            = "Standard_B1s"
+  size                            = "Standard_B2s"
   #custom_data                     = filebase64("userdata/cloud-init.txt")
 
   admin_ssh_key {
